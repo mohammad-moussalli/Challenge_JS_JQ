f05e9e26e41f6e8b3091277ce2b368773123c537
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-01-27 23:56:48 +0200

    Edit randomButton Function

--- challenge2.js
+++ challenge2.js
@@ -18,6 +18,72 @@ $(document).ready(function(){
     }
     $(document).on("keydown", startGame);
 
+    function randomButton(){
+        if(continue_playing == true){
+            var random = Math.floor(Math.random() * 5);
+            var button = 1;
+            if(random == 0){
+                const green_audio = new Audio("sounds/green.mp3");
+                green_audio.play();
+                button = $("#green");
+                button.css("opacity", "0.5");
+                setTimeout(() => {button.css("opacity", "1");
+                }, 100);
+                buttons_memory.push(button);
+            }else if(random == 1){
+                const red_audio = new Audio("sounds/red.mp3");
+                red_audio.play();
+                button = $("#red");
+                button.css("opacity", "0.5");
+                setTimeout(() => {button.css("opacity", "1");
+                }, 100);
+                buttons_memory.push(button);
+            }else if (random == 2) {
+                const yellow_audio = new Audio("sounds/yellow.mp3");
+                yellow_audio.play();
+                button = $("#yellow");
+                button.css("opacity", "0.5");
+                setTimeout(() => {button.css("opacity", "1");
+                }, 100);
+                buttons_memory.push(button);
+            }else{
+                const blue_audio = new Audio("sounds/blue.mp3");
+                blue_audio.play();
+                button = $("#blue");
+                button.css("opacity", "0.5");
+                setTimeout(() => {button.css("opacity", "1");
+                }, 100);
+                buttons_memory.push(button);
+            }
+            for(j = 0; j < buttons_memory.length; j++){
+                if(buttons_memory[j] == $("#green")){
+                    $("#green").on("click", youWin);
+                    $("#blue").on("click", youLose);
+                    $("#red").on("click", youLose);
+                    $("#yellow").on("click", youLose);
+
+                }else if(buttons_memory[j] == $("#red")){
+                    $("#red").on("click", youWin);
+                    $("#blue").on("click", youLose);
+                    $("#green").on("click", youLose);
+                    $("#yellow").on("click", youLose);
+
+                }else if(buttons_memory[j] == $("#yellow")){
+                    $("#yellow").on("click", youWin);
+                    $("#blue").on("click", youLose);
+                    $("#green").on("click", youLose);
+                    $("#red").on("click", youLose);
+                }else if(buttons_memory[j] == $("#blue")){
+                    $("#blue").on("click", youWin);
+                    $("#green").on("click", youLose);
+                    $("#red").on("click", youLose);
+                    $("#yellow").on("click", youLose);
+
+                }
+            }
+        }
+    }
+
     function youWin(){
         win = true;
         continue_playing = true;
@@ -42,65 +108,5 @@ $(document).ready(function(){
         $("#title").text("Game Over, Press Any Key to Restart");
     }
 
-    function randomButton(){
-        if(continue_playing == true){
-            var random = Math.floor(Math.random() * 5);
-            var button = 1;
-            if(random == 0){
-                const blue_audio = new Audio("sounds/blue.mp3");
-                blue_audio.play();
-                button = $("#blue");
-                button.css("opacity", "0.5");
-                setTimeout(() => {button.css("opacity", "1");
-                }, 100);
-                buttons_memory.push(button);
-            }else if(random == 1){
-                const green_audio = new Audio("sounds/green.mp3");
-                green_audio.play();
-                button = $("#green");
-                button.css("opacity", "0.5");
-                setTimeout(() => {button.css("opacity", "1");
-                }, 100);
-                buttons_memory.push(button);
-            }else if(random == 2){
-                const red_audio = new Audio("sounds/red.mp3");
-                red_audio.play();
-                button = $("#red");
-                button.css("opacity", "0.5");
-                setTimeout(() => {button.css("opacity", "1");
-                }, 100);
-                buttons_memory.push(button);
-            }else{
-                const yellow_audio = new Audio("sounds/yellow.mp3");
-                yellow_audio.play();
-                button = $("#yellow");
-                button.css("opacity", "0.5");
-                setTimeout(() => {button.css("opacity", "1");
-                }, 100);
-                buttons_memory.push(button);
-            }
-            for(j = 0; j < buttons_memory.length; j++){
-                if(buttons_memory[j] == $("#blue")){
-                    $("#blue").on("click", youWin);
-                }else{
-                    $("#blue").on("click", youLose);
-                }
-                if(buttons_memory[j] == $("#green")){
-                    $("#green").on("click", youWin);
-                }else{
-                    $("#green").on("click", youLose);
-                }
-                if(buttons_memory[j] == $("#red")){
-                    $("#red").on("click", youWin);
-                }else{
-                    $("#red").on("click", youLose);
-                }
-                if(buttons_memory[j] == $("#yellow")){
-                    $("#yellow").on("click", youWin);
-                }else{
-                    $("#yellow").on("click", youLose);
-                }
-            }
-        }
-    }
+    
 });
